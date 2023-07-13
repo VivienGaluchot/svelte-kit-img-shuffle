a2e59f587bd14303b935f5601a330e7f18977c03
Vivien Galuchot <vivien.galuchot@gmail.com>
2023-07-13 10:37:00 +0000

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import Section from '$lib/layout/section.svelte';
 
+	let image: number;
 	let tileCount: number;
 
 	function play() {
@@ -15,20 +16,24 @@
 
 <Section>
 	<h2>New game</h2>
-
 	<div class="flex-h">
-		<div>
-			<label for="tile-count-select">Level</label>
-			<select id="tile-count-select" bind:value={tileCount}>
-				<option value={50}>Easy</option>
-				<option value={100}>Medium</option>
-				<option value={250}>Hard</option>
-			</select>
+		<div class="flex-h">
+			<div class="flex-h g-sm">
+				<label for="image-select">Image</label>
+				<select id="image-select" bind:value={image}>
+					<option value={1}>Sea</option>
+				</select>
+			</div>
+			<div class="flex-h g-sm">
+				<label for="tile-count-select">Level</label>
+				<select id="tile-count-select" bind:value={tileCount}>
+					<option value={50}>Easy</option>
+					<option value={100}>Medium</option>
+					<option value={250}>Hard</option>
+				</select>
+			</div>
 		</div>
-
-		<div>
-			<button on:click={play}>Play</button>
-		</div>
+		<button on:click={play}>Play</button>
 	</div>
 </Section>
 
@@ -42,6 +47,13 @@
 		flex-wrap: wrap;
 		justify-content: space-between;
 		align-items: center;
+	}
+
+	.flex-h {
 		gap: 1rem;
+	}
+
+	.g-sm {
+		gap: 0.5rem;
 	}
 </style>
