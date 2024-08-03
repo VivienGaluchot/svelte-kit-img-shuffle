d326b83bd65a514c674ec6ee77ccae1786923085
Vivien Galuchot <vivien.galuchot@gmail.com>
2024-08-03 20:07:25 +0000

    fix

---
+++ docs/_app/immutable/chunks/paths.euUD8zNa.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_f4ats)==null?void 0:s.base)??"/svelte-kit-img-shuffle";var t;const a=((t=globalThis.__sveltekit_f4ats)==null?void 0:t.assets)??e;export{a,e as b};
