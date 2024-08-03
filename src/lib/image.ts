e937988ed75824362474cfbda159ed8cf587dbe0
Vivien Galuchot <vivien.galuchot@gmail.com>
2024-08-03 19:54:47 +0000

    add image

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -2,6 +2,7 @@ import type * as lm from '$lib/math';
 
 import image1 from '$lib/assets/photo-1687057217908-54f8e6d30e3c.avif';
 import image2 from '$lib/assets/photo-1689085383650-13d21072f5a6.avif';
+import image3 from '$lib/assets/bruno-martins-GkZvxVsHYWw-unsplash.jpg';
 
 export interface PuzzleImage {
 	name: string;
@@ -18,5 +19,6 @@ export function splitInCell(size: lm.Vec2d, n: number): lm.Vec2d {
 
 export const images: PuzzleImage[] = [
 	{ name: 'Sea', url: image1, size: { x: 930, y: 1162 }, color: "#277d7d" },
-	{ name: 'Town', url: image2, size: { x: 1374, y: 1031 }, color: "#8d8c81" }
+	{ name: 'Town', url: image2, size: { x: 1374, y: 1031 }, color: "#8d8c81" },
+	{ name: 'Street', url: image3, size: {x: 1920, y: 2880 }, color: "#ddd8d5" }
 ];
