cfdaffe065a02bbdc96a7db3b3206d1bc36c5661
Vivien Galuchot <vivien.galuchot@gmail.com>
2024-10-06 15:26:13 +0000

    fix link

---
+++ docs/_app/immutable/chunks/paths.BYxmtUMr.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_k92wnt)==null?void 0:s.base)??"/svelte-kit-img-shuffle";var t;const a=((t=globalThis.__sveltekit_k92wnt)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ docs/_app/immutable/entry/start.Cr8ZRbAV.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DMar-GSC.js";export{t as start};
