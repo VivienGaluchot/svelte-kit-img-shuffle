362b8d9a137f50ea34cb83b6bba58e31558a6c97
Vivien Galuchot <vivien.galuchot@gmail.com>
2024-10-06 16:26:32 +0000

    computed image size

---
+++ docs/_app/immutable/entry/start.B8CyAmzp.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.D8hlazzq.js";export{t as start};
--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,18 +1,19 @@
 import type * as lm from '$lib/math';
+import type { Picture } from 'vite-imagetools';
 
-import image1 from '$lib/assets/photo-1687057217908-54f8e6d30e3c.avif';
-import image2 from '$lib/assets/photo-1689085383650-13d21072f5a6.avif';
-import image3 from '$lib/assets/bruno-martins-GkZvxVsHYWw-unsplash.jpg';
-import image4 from '$lib/assets/alex-gagareen-r8_ZJ3qzgKo-unsplash.jpg';
-import image5 from '$lib/assets/slava-auchynnikau-hAkmZyuHuRE-unsplash.jpg';
-import image6 from '$lib/assets/tara-mae-miller-SzI9z8snVjk-unsplash.jpg';
-import image7 from '$lib/assets/valeria-kodra-2mj8Yu6Vftc-unsplash.jpg';
+import image1 from '$lib/assets/photo-1687057217908-54f8e6d30e3c.avif?enhanced';
+import image2 from '$lib/assets/photo-1689085383650-13d21072f5a6.avif?enhanced';
+import image3 from '$lib/assets/bruno-martins-GkZvxVsHYWw-unsplash.jpg?enhanced';
+import image4 from '$lib/assets/alex-gagareen-r8_ZJ3qzgKo-unsplash.jpg?enhanced';
+import image5 from '$lib/assets/slava-auchynnikau-hAkmZyuHuRE-unsplash.jpg?enhanced';
+import image6 from '$lib/assets/tara-mae-miller-SzI9z8snVjk-unsplash.jpg?enhanced';
+import image7 from '$lib/assets/valeria-kodra-2mj8Yu6Vftc-unsplash.jpg?enhanced';
 
 export interface PuzzleImage {
 	name: string;
-	url: string;
+	picture: Picture;
 	color: string;
-	// in pixel
+	url: string;
 	size: lm.Vec2d;
 }
 
@@ -21,12 +22,22 @@ export function splitInCell(size: lm.Vec2d, n: number): lm.Vec2d {
 	return { x: Math.round(n / y), y: Math.round(y) };
 }
 
+function toPuzzleImage(name: string, picture: Picture, color: string): PuzzleImage {
+	return {
+		name,
+		picture,
+		color,
+		url: picture.img.src,
+		size: { x: picture.img.w, y: picture.img.h }
+	};
+}
+
 export const images: PuzzleImage[] = [
-	{ name: 'Sea', url: image1, size: { x: 930, y: 1162 }, color: "#277d7d" },
-	{ name: 'Town', url: image2, size: { x: 1374, y: 1031 }, color: "#8d8c81" },
-	{ name: 'Street', url: image3, size: {x: 1920, y: 2880 }, color: "#ddd8d5" },
-	{ name: 'Car', url: image4, size: {x: 1920, y: 2872 }, color: "#47949e" },
-	{ name: 'Air ballon', url: image5, size: {x: 1920, y: 2880 }, color: "#f29c63" },
-	{ name: 'Tag', url: image6, size: {x: 1920, y: 1273 }, color: "#f66c87" },
-	{ name: 'Cozy', url: image7, size: {x: 1920, y: 2896 }, color: "#703814" }
+	toPuzzleImage('Sea', image1, '#277d7d'),
+	toPuzzleImage('Town', image2, '#8d8c81'),
+	toPuzzleImage('Street', image3, '#ddd8d5'),
+	toPuzzleImage('Car', image4, '#47949e'),
+	toPuzzleImage('Air ballon', image5, '#f29c63'),
+	toPuzzleImage('Tag', image6, '#f66c87'),
+	toPuzzleImage('Cozy', image7, '#703814')
 ];
