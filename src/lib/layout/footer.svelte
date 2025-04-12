8e06a502b778b1f7026cadc428e8b681afdedfdc
Vivien Galuchot <vivien.galuchot@gmail.com>
2025-04-12 18:48:24 +0000

    add sponsor button

--- src/lib/layout/footer.svelte
+++ src/lib/layout/footer.svelte
@@ -9,23 +9,35 @@
 			href="https://github.com/VivienGaluchot/svelte-kit-img-shuffle"
 			target="_blank"
 		>
-			<i class="fa-brands fa-github"></i> Contribute
+			<i class="fa-solid fa-code-branch"></i>
+			{PUBLIC_VERSION}
 		</a>
 		<a
 			class="button"
 			href="https://github.com/VivienGaluchot/svelte-kit-img-shuffle/issues"
 			target="_blank"
 		>
-			<i class="fa-solid fa-bug"></i> Raise issue
+			<i class="fa-solid fa-bug"></i> Found a bug ?
 		</a>
 	</div>
-	<div>
-		<i class="fa-solid fa-code-branch"></i>
-		{PUBLIC_VERSION}
-	</div>
+	<iframe
+		src="https://github.com/sponsors/VivienGaluchot/button"
+		title="Sponsor VivienGaluchot"
+		height="32"
+		width="114"
+		class="btn"
+		style="border: 0; border-radius: 6px;"
+	></iframe>
 </footer>
 
 <style>
+	a {
+		padding: 0rem 0.3rem;
+		font-size: 0.9rem;
+	}
+	iframe {
+		align-self: center;
+	}
 	footer {
 		text-align: center;
 		font-weight: 200;
