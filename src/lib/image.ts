d550e48f59f416d9977270f87cad778e95e2e2e3
Vivien Galuchot <vivien.galuchot@gmail.com>
2025-05-19 18:38:15 +0000

    complete translation

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,4 +1,5 @@
 import type * as lm from '$lib/math';
+import { m } from '$lib/paraglide/messages.js';
 
 import image1 from '$lib/assets/photo-1687057217908-54f8e6d30e3c.avif';
 import image2 from '$lib/assets/photo-1689085383650-13d21072f5a6.avif';
@@ -37,16 +38,16 @@ export async function toPuzzleImage(res: ImageResource): Promise<PuzzleImage> {
 }
 
 export const staticImages: Record<string, ImageResource> = {
-	sea: { name: 'Sea', url: image1 },
-	town: { name: 'Town', url: image2 },
-	street: { name: 'Street', url: image3 },
-	car: { name: 'Car', url: image4 },
-	air_ballon: { name: 'Air ballon', url: image5 },
-	tag: { name: 'Tag', url: image6 },
-	cosy: { name: 'Cozy', url: image7 },
-	nut: { name: 'Nut', url: image8 },
-	bucks: { name: 'Bucks', url: image9 },
-	image10: { name: 'Cat', url: image10 }
+	sea: { name: m.steep_bad_camel_dash(), url: image1 },
+	town: { name: m.shy_sleek_anaconda_smile(), url: image2 },
+	street: { name: m.odd_tidy_chipmunk_surge(), url: image3 },
+	car: { name: m.fresh_trick_crossbill_grasp(), url: image4 },
+	air_ballon: { name: m.shy_inclusive_ant_gasp(), url: image5 },
+	tag: { name: m.slow_equal_ostrich_win(), url: image6 },
+	cosy: { name: m.brave_chunky_stingray_pinch(), url: image7 },
+	nut: { name: m.zany_fluffy_stingray_urge(), url: image8 },
+	bucks: { name: m.jumpy_actual_midge_buy(), url: image9 },
+	image10: { name: m.cute_petty_pig_pop(), url: image10 }
 };
 
 export function splitInCell(size: lm.Vec2d, n: number): lm.Vec2d {
